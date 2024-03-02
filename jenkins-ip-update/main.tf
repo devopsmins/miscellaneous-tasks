5ce90711e7c2468d991645367851971a495f6141
minhaj <minhajfathima1023@gmail.com>
2024-03-02 05:28:42 -0500

    Prometheus ansible change

---
+++ jenkins-ip-update/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "mins-terraform-state"
+    key    = "misc/jenkins-ip-update/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "jenkins" {
+  instance_id = "i-0e3626aeec70816c8"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins"
+  type    = "A"
+  zone_id = "Z05815251WQO0OK50UPQR"
+  ttl     = 10
+  records = [data.aws_instance.jenkins.public_ip]
+}
