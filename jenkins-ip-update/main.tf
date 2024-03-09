d3c15aa7e8d73f01ff80ffc7a98cd8f88fc418ba
minhaj <minhajfathima1023@gmail.com>
2024-03-09 09:38:39 -0500

    TF code

--- jenkins-ip-update/main.tf
+++ jenkins-ip-update/main.tf
@@ -26,4 +26,17 @@ resource "aws_route53_record" "jenkins" {
 
 }
 
+data "aws_instance" "artifactory" {
+  instance_id = "i-04ce85a3c0997bf29"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory"
+  type    = "A"
+  zone_id = "Z05815251WQO0OK50UPQR"
+  ttl     = 10
+  records = [data.aws_instance.artifactory.public_ip]
+
+}
+
 
