2512a45c2dd8fa9fb188efa6e371655c1c3cf564
minhaj <minhajfathima1023@gmail.com>
2024-04-19 08:27:59 -0400

    TF code

--- jenkins-ip-update/main.tf
+++ jenkins-ip-update/main.tf
@@ -39,3 +39,42 @@ resource "aws_route53_record" "artifactory" {
   records = [data.aws_instance.artifactory.public_ip]
 
 }
+
+data "aws_instance" "elk" {
+  instance_id = "i-0c5e20f1c334b6458"
+}
+
+resource "aws_route53_record" "elk" {
+  name    = "elasticsearch"
+  type    = "A"
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  ttl     = 10
+  records = [data.aws_instance.elk.public_ip]
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "load-gen" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-033d8567b50d2e180"]
+  tags = {
+    Name = "load-gen"
+  }
+}
+
+data "aws_instance" "gocd" {
+  instance_id = "i-038e89f526420eba9"
+}
+
+resource "aws_route53_record" "gocd" {
+  name    = "gocd"
+  type    = "A"
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  ttl     = 10
+  records = [data.aws_instance.gocd.public_ip]
+}
